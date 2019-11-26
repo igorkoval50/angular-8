d0b60bfdcb35a19d4848e18cf134acaab5fb4448
igorkoval50 <kigor2152@gmail.com>
2019-11-26 14:06:59 +0200

    add shipping for the cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,8 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListsComponent } from './product-lists/product-lists.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 
 @NgModule({
@@ -17,13 +20,18 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductListsComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     RouterModule.forRoot([
       { path: '', component: ProductListsComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      {path: 'cart', component: CartComponent},
+      {path: 'shipping', component: ShippingComponent},
     ])
   ],
   providers: [],
